e9961d8c1b2f7bf2d325a5693f8928d630951ecc
sijan <sijan@localhost.localdomain>
2023-06-08 22:59:21 +0545

    updated

--- assets/test.js
+++
@@ -1,9 +0,0 @@
-var elem = document.querySelector('.main-carousel-nerd');
-var flkty = new Flickity( elem, {
-  cellAlign: 'left',
-  contain: true
-});
-
-var flkty = new Flickity( '.main-carousel-nerd', {
-});
-
